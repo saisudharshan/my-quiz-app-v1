08b92c3dd4025a86f256afbebac993a1feb82c7b
saisudharshan <saisudharshan96@gmail.com>
2023-07-26 23:49:33 +0530

    Updates

--- src/Adapter/FetchQuestions.tsx
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-export  const fetchQuestions = () => {
-
-    return axios.get('/data.json').then((res) => {
-        console.log(res)
-      return res.data.questions
-    })
-}
